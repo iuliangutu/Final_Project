6da3e86bc68560158355881b0a8238adfe470e2c
Iulian Gutu <iuliangutu@192.168.1.129>
2025-02-16 12:36:32 +0200

    search_button

--- shop/views.py
+++ shop/views.py
@@ -1,49 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, FormView
-
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.db.models import Q
 from .forms import ProductForm
-from .models import Category, Seller, Product, ProductType, OrderLine, Order, Cart
-
-
-# Create your views here.
+from .models import Category, Product
 
 class ProductView(ListView):
     template_name = 'products.html'
     model = Product
-
     def get_queryset(self):
-        # Start with all products
         products = Product.objects.all()
-
-        # Get rating filter from GET request
+        # Get filters from GET request
         rating = self.request.GET.get('rating', '')
-        if rating:
-            # Filter by rating if specified
-            products = products.filter(rating=rating)
-
-        # Get price sorting option from GET request
         price = self.request.GET.get('price', '')
+        search_query = self.request.GET.get('search', '')
+        # Apply rating filter
+        if rating:
+            products = products.filter(rating=rating)
+        # Apply price sorting
         if price == 'ascending':
-            products = products.order_by('price')  # Ascending order
+            products = products.order_by('price')
         elif price == 'descending':
-            products = products.order_by('-price')  # Descending order
-
+            products = products.order_by('-price')
+        # Apply search filter
+        if search_query:
+            products = products.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
         return products
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add the rating and price_order to context so they can be used in the template
+        # Add the rating, price, and search to context so they can be used in the template
         context['rating'] = self.request.GET.get('rating', '')
-        context['price_order'] = self.request.GET.get('price_order', '')
+        context['price'] = self.request.GET.get('price', '')
+        context['search'] = self.request.GET.get('search', '')
         return context
-
-
 
 class ProductViewDetail(DetailView):
     template_name = 'products_detail.html'
     model = Product
-
 
 class CategoryProductView(ListView):
     template_name = 'products.html'
@@ -51,44 +47,48 @@ class CategoryProductView(ListView):
     def get_queryset(self):
         category = Category.objects.get(name=self.kwargs['category_name'])
         products = Product.objects.filter(category=category)
-        # Get rating filter from GET request
+        # Get filters from GET request
         rating = self.request.GET.get('rating', '')
-        if rating:
-            # Filter by rating if specified
-            products = products.filter(rating=rating)
-        # Get price sorting option from GET request
         price = self.request.GET.get('price', '')
+        search_query = self.request.GET.get('search', '')
+        # Apply rating filter
+        if rating:
+            products = products.filter(rating=rating)
+        # Apply price sorting
         if price == 'ascending':
-            products = products.order_by('price')  # Ascending order
+            products = products.order_by('price')
         elif price == 'descending':
-            products = products.order_by('-price')  # Descending order
+            products = products.order_by('-price')
+        # Apply search filter
+        if search_query:
+            products = products.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
         return products
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         category = Category.objects.get(name=self.kwargs['category_name'])
         context['category_name'] = category.name
         context['rating'] = self.request.GET.get('rating', '')
-        context['price_order'] = self.request.GET.get('price', '')
+        context['price'] = self.request.GET.get('price', '')
+        context['search'] = self.request.GET.get('search', '')
         return context
 
 class CategoryView(ListView):
     template_name = 'categories.html'
     model = Category
 
-
 class ProductCreateView(CreateView):
     template_name = 'form.html'
     form_class = ProductForm
-
     def form_valid(self, form):
         form.instance.image = self.request.FILES.get('image')
         return super().form_valid(form)
-
     def form_invalid(self, form):
         print("User provided invalid data")
-        # print(form.errors)
         return super().form_invalid(form)
-
 
 class ProductUpdateView(UpdateView):
     template_name = 'form.html'
@@ -96,17 +96,14 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
     success_url = reverse_lazy('product-list')
 
-
 class ProductDeleteView(DeleteView):
     template_name = 'product_confirm_delete.html'
     model = Product
     success_url = reverse_lazy('product-list')
 
-
 class OrderView(ListView):
     template_name = 'products.html'
     model = Product
-
 
 def about(request):
     return render(request, 'about.html')
@@ -116,4 +113,3 @@ def contact(request):
 
 def privacy(request):
     return render(request, 'privacy.html')
-
